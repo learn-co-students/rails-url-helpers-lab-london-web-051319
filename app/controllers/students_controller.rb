d81e8fd2212f3f426b29301c90ab66e3715f64d5
github username <github email address>
2019-06-06 17:19:38 +0100

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,12 +1,43 @@
 class StudentsController < ApplicationController
-  before_action :set_student, only: :show
-  
+  before_action :set_student, only: [:show, :edit, :update, :destroy, :activate]
+  skip_before_action :verify_authenticity_token
+
   def index
     @students = Student.all
   end
 
   def show
+     @student = Student.find(params[:id])
   end
+
+  def new
+  end
+
+  def create
+          @student = Student.create(first_name: params["first_name"], last_name: params["second_name"], created_at: params["created_at"].to_datetime, updated_at: params["updated_at"].to_datetime)
+          redirect_to student_path(@student)
+  end
+
+  def edit
+  end
+
+  def update
+     if params[:first_name]
+        @student.update(first_name: params["first_name"], last_name: params["second_name"], updated_at: params["updated_at"].to_datetime)
+     else
+        @student.update(active: params[:active], updated_at: params["updated_at"].to_datetime)
+     end
+    redirect_to student_path
+  end
+
+  def activate
+  end
+
+  def destroy
+     @student.destroy
+     redirect_to students_path
+  end
+
 
   private
 
--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,7 @@
 # Load the Rails application.
 require_relative 'application'
+require "pry-rails"
+require 'pry'
 
 # Initialize the Rails application.
 Rails.application.initialize!
