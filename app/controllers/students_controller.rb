a3f10f0251bbeadf9bc84f9f8c1651296e758872
mbarrins <maija.barrins@gmail.com>
2019-06-06 11:14:33 +0100

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -6,6 +6,13 @@ class StudentsController < ApplicationController
   end
 
   def show
+    set_student
+  end
+
+  def activate
+    set_student
+    @student.toggle_active
+    redirect_to student_path(@student)
   end
 
   private
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,5 +1,19 @@
 class Student < ActiveRecord::Base
+  after_initialize :default_values
+
+  def default_values
+    self.active ||= false
+  end
+
   def to_s
     self.first_name + " " + self.last_name
+  end
+
+  def active_to_s
+    "This student is currently #{self.active ? 'active' : 'inactive'}."
+  end
+
+  def toggle_active
+    self.update(active: !self.active)
   end
 end
